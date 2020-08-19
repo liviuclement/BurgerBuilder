984ea11f1eb8e563f55707f7611bfba36158d90c
Liviu Clement <liviu_clement@yahoo.com>
2020-08-19 16:25:30 +0300

    Updates

---
+++ precache-manifest.f0923624298180c44db1934931116b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df89bba39aad8c7a2bb70d05bbeccffb",
+    "url": "/liviuclement.github.io/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "be451701440c14413371",
+    "url": "/liviuclement.github.io/BurgerBuilder/static/css/main.78f29f4c.chunk.css"
+  },
+  {
+    "revision": "0577c4a76314c7adead1",
+    "url": "/liviuclement.github.io/BurgerBuilder/static/js/2.2f373e3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/liviuclement.github.io/BurgerBuilder/static/js/2.2f373e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be451701440c14413371",
+    "url": "/liviuclement.github.io/BurgerBuilder/static/js/main.d620d432.chunk.js"
+  },
+  {
+    "revision": "2988ee9a25ff356f491a",
+    "url": "/liviuclement.github.io/BurgerBuilder/static/js/runtime-main.12c29e12.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/liviuclement.github.io/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
